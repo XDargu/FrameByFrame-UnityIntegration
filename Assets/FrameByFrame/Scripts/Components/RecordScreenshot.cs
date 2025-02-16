35027e2e643d5e9eb8abd243dda00e544b0eb120
XDargu <xdargu@gmail.com>
2025-02-16 23:32:26 +0100

    Rename Recording Scripts

--- Assets/FrameByFrame/Scripts/Components/RecordScreenshot.cs
+++ Assets/FrameByFrame/Scripts/Components/RecordScreenshot.cs
@@ -16,17 +16,20 @@ public class RecordScreenshot : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-		// Add screenshot
-		string screenshotName =
-								"Screenshot_" +
-								(UInt32)Time.frameCount +
-								".png";
+		if (Application.isPlaying)
+		{
+			// Add screenshot
+			string screenshotName =
+									"Screenshot_" +
+									(UInt32)Time.frameCount +
+									".png";
+			
+			Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+			byte[] jpgTex = tex.EncodeToJPG(35);
+			FbFManager.RecordResource(screenshotName, "image/jpg", "{\"blob\":\"data:image/jpg;base64," + Convert.ToBase64String(jpgTex) + "\" }");
 
-		Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
-		byte[] jpgTex = tex.EncodeToJPG(35);
-		FbFManager.RecordResource(screenshotName, "image/jpg", "{\"blob\":\"data:image/jpg;base64," + Convert.ToBase64String(jpgTex) + "\" }");
-
-		PropertyGroup entity = FbFManager.RecordProperties(this.gameObject, "Screenshot");
-		entity.AddPlane("Plane", gameObject.transform.position, gameObject.transform.up, gameObject.transform.forward, tex.width / 1000.0f, tex.height / 1000.0f, new Color(1, 1, 1, 1), screenshotName, "ShapeHelpers");
+			PropertyGroup entity = FbFManager.RecordProperties(this.gameObject, "Screenshot");
+			entity.AddPlane("Plane", gameObject.transform.position, gameObject.transform.up, gameObject.transform.forward, tex.width / 1000.0f, tex.height / 1000.0f, new Color(1, 1, 1, 1), screenshotName, "ShapeHelpers");
+		}
 	}
 }
